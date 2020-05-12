b7546cfd5c1b1268a1b4f173b0a25c7eeb32fc7f
Jared K <kel03009@byui.edu>
2020-05-11 22:11:31 -0500

    messed with test php

--- assignments/week03/test.php
+++ assignments/week03/test.php
@@ -1,14 +1,24 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Testing PHP Skills</title>
+    <style media="screen">
+      a{color:#777;}
+      a:hover{color: tomato;}
+      body{font-size: 2rem;font-family: arial;color:#777;}
+    </style>
   </head>
   <body>
-
+    <a href="test.html">HTML page</a>
     <?php
-      echo "Your name is " . $_SESSION["userName"];
+      echo "<br><br>This is where the PHP goes.";
 
-    ?>
+      $_SESSION['name'] = htmlspecialchars($_GET['username']);
+
+      echo "<br><br>" . $_SESSION['name'];
+
+     ?>
   </body>
 </html>
