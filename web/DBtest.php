d1029fbf6f535467b6aaacaa8712b18376fb33d8
Jared K <kel03009@byui.edu>
2020-05-21 22:21:47 -0500

    did sumting

--- web/DBtest.php
+++ web/DBtest.php
@@ -10,5 +10,12 @@
 </head>
 <body>
 	Use this to test DB interactions.
+	<?php  
+		$statement = $db->query('SELECT title FROM songs ORDER BY title ASC;');
+		$results = $statement->fetch(PDO::FETCH_ASSOC);
+
+		echo "<div>".$results."</div>";
+
+	?>
 </body>
 </html>
