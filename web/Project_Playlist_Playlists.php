9f2ed46d37f7bdba256bedac191498e849950a7a
Jared K <kel03009@byui.edu>
2020-05-25 18:06:01 -0500

    playlist page

---
+++ web/Project_Playlist_Playlists.php
@@ -0,0 +1,38 @@
+<?php
+	require "DBConnection.php";
+	$db = get_db();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Road-Trip Playlist Maker</title>
+	<link rel="stylesheet" href="projectStyle.css" />
+
+</head>
+<body>
+	<header>
+		<h1>ROAD-TRIP PLAYLIST MAKER</h1>
+	</header>
+	<a href="Project_Playlist.html"><h3>Back to start page</h3></a>
+	<div>
+		<a href="Project_Playlist_Artists.php">
+			<input type="button" name="artists" value="Artists" id="artists">
+		</a>
+		<a href="Project_Playlist_Albums.php">
+			<input type="button" name="albums" value="Albums" id="albums">
+		</a>
+		<a href="Project_Playlist_Songs.php">
+			<input type="button" name="songs" value="Songs" id="songs">
+		</a>
+	</div>
+
+	<div id="results">
+		<?php  
+			echo "<p>All selected songs for the playlist should go here. Also show TOTAL PLAY TIME, UserName, date created, and playlistTitle</p>"
+		?>
+	</div>
+
+	<script type="Project_Playlist.js"></script>
+</body>
+</html>
