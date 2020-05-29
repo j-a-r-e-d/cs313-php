d078883cf639d8f2d7cc216eb1a8e28895e9074a
Jared K <kel03009@byui.edu>
2020-05-28 22:35:28 -0500

    fingers crossed, watson.

--- web/Project_Playlist_Artists.php
+++ web/Project_Playlist_Artists.php
@@ -1,14 +1,27 @@
 <?php
-	
+	// ASSIGN VARIABLE
 	if (!isset($_GET['genreID']))
 	{
 		die("Error, genre id not specified...");
 	}
-
+	// ESCAPE ANY MALICIOUS CHARACTERS IN THE INPUT VARIABLE
 	$genreID = htmlspecialchars($_GET['genreID']);
 
+	//CONNECT TO THE DATABASE
 	require "DBConnection.php";
 	$db = get_db();
+
+	// PREPARE STATEMENT
+	$statement = $db->prepare('
+		SELECT DISTINCT r.artistName 
+		FROM artists r
+		JOIN albums a ON a.artistid = r.artistid
+		AND a.genreid = :id
+		ORDER BY artistName;');
+	$statement->bindValue(':id', $genreID, PDO::PARAM_INT);
+	$statement->execute();
+	$artists = $statement->fetchAll(PDO::FETCH_ASSOC);
+	$cnt = 0;
 ?>
 
 <!DOCTYPE html>
@@ -24,38 +37,45 @@
 	</header>
 	<a href="Project_Playlist.html"><h3>Back to start page</h3></a>
 	<div>
-		<!-- <a href="Project_Playlist_Artists.php"> -->
+		<table name="artists" id="artists">
+			<tr>
+				<th>Artists</th>
+			</tr>
+		<?php
+			
+		foreach ($artists as $artist) {
+			$artistID = $artist['artistid'];
+			$artistname = $artist['artistname']; 
+			
+			echo "<tr><td><a href='Project_Playlist_Albums.php?genreID=$genreID&artistID=$artistID'>$artistname</a></td></tr>";
+		}
+
+		?>
+
+		</table>
+<!-- 		<a href="Project_Playlist_Artists.php">
 			<input type="button" name="artists" value="Artists" id="artists">
-		<!-- </a> -->
-		<!-- <a href="Project_Playlist_Albums.php"> -->
+		</a>
+		<a href="Project_Playlist_Albums.php">
 			<input type="button" name="albums" value="Albums" id="albums">
-		<!-- </a> -->
-		<!-- <a href="Project_Playlist_Songs.php"> -->
+		</a>
+		<a href="Project_Playlist_Songs.php">
 			<input type="button" name="songs" value="Songs" id="songs">
-		<!-- </a> -->
-		<!-- <a href="Project_Playlist_Playlists.php"> -->
+		</a>
+		<a href="Project_Playlist_Playlists.php">
 			<input type="button" name="playlists" value="Playlists" id="playlists">
-		<!-- </a> -->
-	</div>
+		</a>
+ -->	</div>
 
 	<div id="results">
 		<?php  
-			$statement = $db->prepare('
-				SELECT DISTINCT r.artistName 
-				FROM artists r
-				JOIN albums a ON a.artistid = r.artistid
-				AND a.genreid = :id
-				ORDER BY artistName;');
-			$statement->bindValue(':id', $genreID, PDO::PARAM_INT);
-			$statement->execute();
-			$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-			$cnt = 0;
+			
 
-			foreach ($results as $row) {
+			foreach ($artists as $artist) {
 				$cnt++;
-				$artistName = $row['artistname']; // I had to change column name (artistname) 
+				$artistName = $artist['artistname']; // I had to change column name (artistname) 
 												  // to all lowercase.
-				echo $cnt.'. '.'<span style="color:#777;">'.$artistName.'</span><br>';
+				echo $cnt.'. $artistName<br>';
 			}
 		?>
 	</div>
