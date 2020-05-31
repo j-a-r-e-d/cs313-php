1c9822f92be49649918bc3cdca7cca66b1bdc46f
Jared K <kel03009@byui.edu>
2020-05-31 16:48:18 -0500

    .

--- web/insert_tester.php
+++ web/insert_tester.php
@@ -12,6 +12,23 @@ require 'DBConnection.php';
 $db = get_db();
 
 clog('Database connection completed....');
+
+// Let's get the variables assigned...
+$user_name = htmlspecialchars($_GET['user_name']);
+$user_DOB = htmlspecialchars($_GET['user_DOB']);
+$user_email = htmlspecialchars($_GET['user_email']);
+
+//PREPARE STATEMENT
+$statement = $db->prepare('
+	INSERT INTO insert_tester (user_name,user_dob,user_email)
+	VALUES (:user_name, :user_DOB, :user_email)');
+$statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+$statement->bindValue(':user_DOB',$user_DOB,PDO::PARAM_STR);
+$statement->bindValue(':user_email',$user_email,PDO::PARAM_STR);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+clog('Prepared statement copmleted...');
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,9 +38,27 @@ clog('Database connection completed....');
 <body>
 	<h3>INSERT TESTER PHP PAGE</h3>
 	<p>
+		<table>
+			<tr>
+				<th>Name</th>
+				<th>DOB</th>
+				<th>Email</th>
+			</tr>
 		<?php
-		echo 'echoed a message here.';
+		foreach ($results as $row) {
+			$user_name = $row['user_name'];
+			$user_DOB = $row['user_dob'];
+			$user_email = $row['user_email'];
+			$cnt = 1; 
+			
+			
+			echo "<tr><td>$cnt. $user_name</td><td>$user_DOB</td><td>$user_email</td></tr>";
+
+			$cnt++;
+		}
 		?>
+			
+		</table>
 	</p>
 </body>
 </html>
