0d691a79802b15da954ea2aae5015dcb3097aded
Jared K <kel03009@byui.edu>
2020-05-31 10:40:46 -0500

    .

---
+++ web/insert_tester.php
@@ -0,0 +1,18 @@
+<?php
+	include ChromePhp.php;
+	require DBConnection.php;
+	$db = get_db();
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Database Insert Tester</title>
+</head>
+<body>
+	<p>
+		Create a FORM that will take user_name, user_DOB, and user_email input and insert it into the database.
+	</p>
+</body>
+</html>
