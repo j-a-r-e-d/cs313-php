9e2b161c1f0cb6b7e8e0ef3197a9cece609d5fba
Jared K <kel03009@byui.edu>
2020-05-31 15:34:30 -0500

    .

--- web/insert_tester.php
+++ web/insert_tester.php
@@ -1,23 +1,13 @@
-<!-- <?php
-	//include ChromePhp.php;
-	// require DBConnection.php;
-	// $db = get_db();
-
-	//function clog($x) {
-	//	ChromePhp::log($x);
-	}
-
-	//clog('ChromePhp.php included successfully...');
-?> -->
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Database Insert Tester</title>
 </head>
 <body>
+	<h3>INSERT TESTER PHP PAGE</h3>
 	<p>
 		<?php
-		echo 'Create a FORM that will take user_name, user_DOB, and user_email input and insert it into the database.';
+		echo 'echoed a message here.';
 		?>
 	</p>
 </body>
