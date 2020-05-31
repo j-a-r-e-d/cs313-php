5dea0b4c14fd4912428aedbc8e485d519c30948f
Jared K <kel03009@byui.edu>
2020-05-30 20:38:13 -0500

    got problems...

--- web/Project_Playlist_Albums.php
+++ web/Project_Playlist_Albums.php
@@ -104,7 +104,7 @@
 		foreach ($albums as $album) {
 			$albumID = $album['albumid'];
 			$albumTitle = $album['title']; 
-			echo "<tr><td><a href='Project_Playlist_Songs.php?genreID=$genreID&genreDesc=$genreDesc&artistID=$artistID&artistname=$artistname&albumID=$albumID&albumTitle=$albumTitle'>$albumTitle</a></td></tr>";
+			//echo "<tr><td><a href='Project_Playlist_Songs.php?genreID=$genreID&genreDesc=$genreDesc&artistID=$artistID&artistname=$artistname&albumID=$albumID&albumTitle=$albumTitle'>$albumTitle</a></td></tr>";
 		}
 		
 		echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$genreDesc'><input type='button' id='goBack' value='Go Back'></a></td></tr>"
