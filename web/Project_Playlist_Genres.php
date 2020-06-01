7f41c7756758cf78a42c69df9bfd0c7c789f1d28
Jared K <kel03009@byui.edu>
2020-05-31 23:06:30 -0500

    .

--- web/Project_Playlist_Genres.php
+++ web/Project_Playlist_Genres.php
@@ -58,57 +58,57 @@
 	clog('$email = '.$email);
 	clog('$travelTime = '.$travelTime);
 
-	// // INSERT ALL THE USER INFORMATION....
-	// // Address insert
-	// $insert_address_stmt = $db->prepare("
-	// 	INSERT INTO address (city,stateid,datecreated,isdeleted)
-	// 	VALUES (:city,(SELECT stateid FROM states WHERE statecode = :state),date(now()),'f'	);");
-	// $insert_address_stmt->bindValue(':city', $city, PDO::PARAM_STR);
-	// $insert_address_stmt->bindValue(':state',$state,PDO::PARAM_STR);
-	// $insert_address_stmt->execute();
+	// INSERT ALL THE USER INFORMATION....
+	// Address insert
+	$insert_address_stmt = $db->prepare("
+		INSERT INTO address (city,stateid,datecreated,isdeleted)
+		VALUES (:city,(SELECT stateid FROM states WHERE statecode = :state),date(now()),'f'	);");
+	$insert_address_stmt->bindValue(':city', $city, PDO::PARAM_STR);
+	$insert_address_stmt->bindValue(':state',$state,PDO::PARAM_STR);
+	$insert_address_stmt->execute();
 
-	// clog('prepare statement created...');
-	// clog('bindValues set...');
-	// clog('Address insert executed...');
+	clog('prepare statement created...');
+	clog('bindValues set...');
+	clog('Address insert executed...');
 
-	// // User insert
-	// $insert_user_stmt = $db->prepare("
-	// 	INSERT INTO music_users (
-	// 	firstname,
-	// 	lastname,
-	// 	loginname,
-	// 	addressid,
-	// 	email,
-	// 	travelmins,
-	// 	datecreated,
-	// 	isdeleted)
-	// 	VALUES (
-	// 	:firstname,
-	// 	:lastname,
-	// 	:loginname,
-	// 	(SELECT currval('dupe_address_addressid_seq')),
-	// 	:email,
-	// 	:travelmins, 
-	// 	date(now()),
-	// 	'f'	);");
-	// $insert_user_stmt->bindValue(':firstname', $firstName, PDO::PARAM_STR);
-	// $insert_user_stmt->bindValue(':lastname',$lastName,PDO::PARAM_STR);
-	// $insert_user_stmt->bindValue(':loginname',$loginName,PDO::PARAM_STR);
-	// $insert_user_stmt->bindValue(':email',$email,PDO::PARAM_STR);
-	// $insert_user_stmt->bindValue(':travelmins',$travelTime,PDO::PARAM_INT);
-	// $insert_user_stmt->execute();
+	// User insert
+	$insert_user_stmt = $db->prepare("
+		INSERT INTO music_users (
+		firstname,
+		lastname,
+		loginname,
+		addressid,
+		email,
+		travelmins,
+		datecreated,
+		isdeleted)
+		VALUES (
+		:firstname,
+		:lastname,
+		:loginname,
+		(SELECT currval('address_addressid_seq')),
+		:email,
+		:travelmins, 
+		date(now()),
+		'f'	);");
+	$insert_user_stmt->bindValue(':firstname', $firstName, PDO::PARAM_STR);
+	$insert_user_stmt->bindValue(':lastname',$lastName,PDO::PARAM_STR);
+	$insert_user_stmt->bindValue(':loginname',$loginName,PDO::PARAM_STR);
+	$insert_user_stmt->bindValue(':email',$email,PDO::PARAM_STR);
+	$insert_user_stmt->bindValue(':travelmins',$travelTime,PDO::PARAM_INT);
+	$insert_user_stmt->execute();
 
-	// clog('User insert completed...');
+	clog('User insert completed...');
 
-	// // GET ALL THE GENRE DATA FROM THE DATABASE...
-	// $query = "	SELECT genreid, description 
-	// 			FROM genres
-	// 			ORDER BY description;" ;
-	// $stmt = $db->prepare($query);
-	// $stmt->execute();
-	// $genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	// GET ALL THE GENRE DATA FROM THE DATABASE...
+	$query = "	SELECT genreid, description 
+				FROM genres
+				ORDER BY description;" ;
+	$stmt = $db->prepare($query);
+	$stmt->execute();
+	$genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	// clog('Genres query executed and data fetched...');
+	clog('Genres query executed and data fetched...');
 
 ?>
 <!DOCTYPE html>
@@ -127,13 +127,13 @@
 				<th>Genres</th>
 			</tr>
 			<?php
-			// foreach ($genres as $genre) {
-			// 	$genreID = $genre['genreid'];
-			// 	$description = $genre['description']; 
-			// 	echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$description'>$description</a></td></tr>";
-			// 	$text = 'GenreID = '.$genreID.' GenreDesc = '.$description;
-			// 	clog($text);
-			// }
+			foreach ($genres as $genre) {
+				$genreID = $genre['genreid'];
+				$description = $genre['description']; 
+				echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$description'>$description</a></td></tr>";
+				$text = 'GenreID = '.$genreID.' GenreDesc = '.$description;
+				clog($text);
+			}
 			?>
 		</table>
 	</div>
