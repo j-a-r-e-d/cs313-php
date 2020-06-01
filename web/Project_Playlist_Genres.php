8dfa9895274de91f7ce30a8244614f229ebf0421
Jared K <kel03009@byui.edu>
2020-05-31 23:00:27 -0500

    .

--- web/Project_Playlist_Genres.php
+++ web/Project_Playlist_Genres.php
@@ -1,12 +1,13 @@
 <?php
-	require "DBConnection.php";
+	include 'ChromePhp.php';
+	require 'DBConnection.php';
 	$db = get_db();
 
-	// Include php file for commenting in console and create a shorthand function for logging...
-	include 'ChromePhp.php';
 	function clog($x) { // 'clog' short for 'console log'
 		ChromePhp::log($x);
 	}
+
+	clog('created clog() successfully...');
 
 	// CHECK AND ASSIGN VARIABLES...
 	if (!isset($_GET['firstName']))
@@ -38,6 +39,8 @@
 		die("Error, travel time not specified...");
 	}
 
+	clog('Input variable checked....');
+
 	$firstName = htmlspecialchars($_GET['firstName']);
 	$lastName = htmlspecialchars($_GET['lastName']);
 	$loginName = htmlspecialchars($_GET['loginName']);
@@ -46,21 +49,30 @@
 	$email = htmlspecialchars($_GET['email']);
 	$travelTime = htmlspecialchars($_GET['travelTime']);
 
+	clog('Variables assigned...');
+	clog('$firstName = '.$firstName);
+	clog('$lastName = '.$lastName);
+	clog('$loginName = '.$loginName);
+	clog('$city = '.$city);
+	clog('$state = '.$state);
+	clog('$email = '.$email);
+	clog('$travelTime = '.$travelTime);
+
 	// INSERT ALL THE USER INFORMATION....
 	// Address insert
-	$insert_address_stmt = $db->prepare('
+	$insert_address_stmt = $db->prepare("
 		INSERT INTO address (city,stateid,datecreated,isdeleted)
-		VALUES (
-		:city,
-		(SELECT stateid FROM states WHERE statecode = :state), 
-		date(now()),
-		'f'
-		);');
+		VALUES (:city,(SELECT stateid FROM states WHERE statecode = :state),date(now()),'f'	);");
 	$insert_address_stmt->bindValue(':city', $city, PDO::PARAM_STR);
 	$insert_address_stmt->bindValue(':state',$state,PDO::PARAM_STR);
 	$insert_address_stmt->execute();
+
+	clog('prepare statement created...');
+	clog('bindValues set...');
+	clog('Address insert executed...');
+
 	// User insert
-	$insert_user_stmt = $db->prepare('
+	$insert_user_stmt = $db->prepare("
 		INSERT INTO music_users (
 		firstname,
 		lastname,
@@ -74,18 +86,19 @@
 		:firstname,
 		:lastname,
 		:loginname,
-		(SELECT last_value FROM address_addressid_seq),
+		(SELECT currval('dupe_address_addressid_seq')),
 		:email,
 		:travelmins, 
 		date(now()),
-		'f'
-		);');
+		'f'	);");
 	$insert_user_stmt->bindValue(':firstname', $firstName, PDO::PARAM_STR);
 	$insert_user_stmt->bindValue(':lastname',$lastName,PDO::PARAM_STR);
 	$insert_user_stmt->bindValue(':loginname',$loginName,PDO::PARAM_STR);
 	$insert_user_stmt->bindValue(':email',$email,PDO::PARAM_STR);
 	$insert_user_stmt->bindValue(':travelmins',$travelTime,PDO::PARAM_INT);
 	$insert_user_stmt->execute();
+
+	clog('User insert completed...');
 
 	// GET ALL THE GENRE DATA FROM THE DATABASE...
 	$query = "	SELECT genreid, description 
@@ -95,47 +108,34 @@
 	$stmt->execute();
 	$genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
+	clog('Genres query executed and data fetched...');
 
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Road-Trip Playlist Maker</title>
 	<link rel="stylesheet" href="projectStyle.css" />
-
 </head>
 <body>
 	<header>
 		<h1>ROAD-TRIP PLAYLIST MAKER</h1>
 	</header>
-	<a href="Project_Playlist.html"><h3 id="goBackHome">Back to start page</h3></a>
-
 	<div id="buttonMenu">
-
 		<table name="genres" id="genres">
 			<tr>
 				<th>Genres</th>
 			</tr>
-		<?php
-		clog("The PHP console extension is working....");
-		foreach ($genres as $genre) {
-			$genreID = $genre['genreid'];
-			$description = $genre['description']; 
-			//echo "<a href='Project_Playlist_Artists.php' target='_blank'>Albums</a><br>";
-			echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$description'>$description</a></td></tr>";
-			$text = 'GenreID = '.$genreID.' GenreDesc = '.$description;
-			clog($text);
-		}
-
-		?>
-
+			<?php
+			foreach ($genres as $genre) {
+				$genreID = $genre['genreid'];
+				$description = $genre['description']; 
+				echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$description'>$description</a></td></tr>";
+				$text = 'GenreID = '.$genreID.' GenreDesc = '.$description;
+				clog($text);
+			}
+			?>
 		</table>
-		
 	</div>
-
-	<div id="results">
-	</div>
-
-	<script type="Project_Playlist.js"></script>
 </body>
 </html>
