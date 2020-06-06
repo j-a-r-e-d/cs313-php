dfa50b1c2a25e5a88fadd095c1d7519a477f1f93
Jared K <kel03009@byui.edu>
2020-06-06 17:53:54 -0500

    .

---
+++ web/Project_Playlist_GenresRevisit.php
@@ -0,0 +1,106 @@
+<?php
+	include 'ChromePhp.php';
+	require 'DBConnection.php';
+	$db = get_db();
+
+	function clog($x) { // 'clog' short for 'console log'
+		ChromePhp::log($x);
+	}
+
+	clog('created clog() successfully...');
+
+	// CHECK AND ASSIGN VARIABLES...
+	if (!isset($_GET['firstName']))
+	{
+		die("Error, first name not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['lastName']))
+	{
+		die("Error, last name not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['loginName']))
+	{
+		die("Error, login name not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['city']))
+	{
+		die("Error, city not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['state']))
+	{
+		die("Error, state not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['email']))
+	{
+		die("Error, email not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['travelTime']))
+	{
+		die("Error, travel time not specified...");
+		clog("Error, first name not specified...");
+	}
+
+	clog('Input variable checked....');
+
+	$firstName 	= htmlspecialchars($_GET['firstName']);
+	$lastName 	= htmlspecialchars($_GET['lastName']);
+	$loginName 	= htmlspecialchars($_GET['loginName']);
+	$city 		= htmlspecialchars($_GET['city']);
+	$state 		= htmlspecialchars($_GET['state']);
+	$email 		= htmlspecialchars($_GET['email']);
+	$travelTime = htmlspecialchars($_GET['travelTime']);
+
+	clog('Variables assigned...');
+	clog('$firstName = '.$firstName);
+	clog('$lastName = '.$lastName);
+	clog('$loginName = '.$loginName);
+	clog('$city = '.$city);
+	clog('$state = '.$state);
+	clog('$email = '.$email);
+	clog('$travelTime = '.$travelTime);
+
+	// GET ALL THE GENRE DATA FROM THE DATABASE...
+	$query = "	SELECT genreid, description 
+				FROM genres
+				ORDER BY description;" ;
+	$stmt = $db->prepare($query);
+	$stmt->execute();
+	$genres = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	clog('Genres query executed and data fetched...');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Road-Trip Playlist Maker</title>
+	<link rel="stylesheet" href="projectStyle.css" />
+</head>
+<body>
+	<header>
+		<h1>ROAD-TRIP PLAYLIST MAKER</h1>
+	</header>
+	<div id="buttonMenu">
+		<table name="genres" id="genres">
+			<tr>
+				<th>Genres</th>
+			</tr>
+			<?php
+			foreach ($genres as $genre) {
+				$genreID = $genre['genreid'];
+				$description = $genre['description']; 
+				echo "<tr><td><a href='Project_Playlist_Artists.php?genreID=$genreID&genreDesc=$description'>$description</a></td></tr>";
+				$text = 'GenreID = '.$genreID.' GenreDesc = '.$description;
+				clog($text);
+			}
+			?>
+		</table>
+	</div>
+</body>
+</html>
