0201bf43a8e4851a7ebb0807403a10bb81adaea0
Jared K <kel03009@byui.edu>
2020-06-07 17:23:10 -0500

    .

--- db/scratchpad.sql
+++ db/scratchpad.sql
@@ -2,10 +2,11 @@
 
 SELECT	p.title AS Playlist,
 		p.datecreated AS DateCreated,
-		(	SELECT SUM(seconds) 
-			FROM songs 
-			JOIN playlists p ON p.songid = s.songid 
-			WHERE p.title = :playlistTitle) AS Playlist_Duration,
+		COALESCE((	SELECT SUM(seconds) 
+					FROM songs 
+					JOIN playlists_detail pd ON pd.songid = s.songid 
+					JOIN playlists p ON p.playlistid = pd.playlistid
+					WHERE p.title = :playlistTitle),0) AS Playlist_Duration,
 		COALESCE(s.title,'') AS Song_Title,
 		s.seconds::VARCHAR(4) AS Song_Duration,
 		mu.firstname||' '||mu.lastname AS Created_By
@@ -14,4 +15,21 @@ JOIN 	music_users 		mu ON 	mu.userid = p.userid
 LEFT JOIN playlists_detail 	pd ON 	pd.playlistid = p.playlistid
 LEFT JOIN songs 			s ON	s.songid = pd.songid 
 WHERE 	p.isdeleted = 'f' AND
-		p.title = :playlistTitle
+		p.title = :playlistTitle;
+
+SELECT	p.title AS Playlist,
+		p.datecreated AS DateCreated,
+		COALESCE((	SELECT SUM(seconds) 
+					FROM songs 
+					JOIN playlists_detail pd ON pd.songid = s.songid 
+					JOIN playlists p ON p.playlistid = pd.playlistid
+					WHERE p.title = 'Revolution Tunes'),0) AS Playlist_Duration,
+		COALESCE(s.title,'') AS Song_Title,
+		s.seconds::VARCHAR(4) AS Song_Duration,
+		mu.firstname||' '||mu.lastname AS Created_By
+FROM 	playlists 			p 
+JOIN 	music_users 		mu ON 	mu.userid = p.userid
+LEFT JOIN playlists_detail 	pd ON 	pd.playlistid = p.playlistid
+LEFT JOIN songs 			s ON	s.songid = pd.songid 
+WHERE 	p.isdeleted = 'f' AND
+		p.title = 'Revolution Tunes';
