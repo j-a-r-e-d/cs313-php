0a21f99d0b13fa57d20c9c0b4df0a8d3e239c53c
Jared K <kel03009@byui.edu>
2020-06-07 15:58:09 -0500

    .

--- web/Project_Playlist_MyPlaylist.php
+++ web/Project_Playlist_MyPlaylist.php
@@ -1,8 +1,125 @@
 <?php
+	include 'ChromePhp.php';
+	function clog($x) { // 'clog' short for 'console log'
+		ChromePhp::log($x);
+	}
+
+	// Convert seconds to Human Readable time
+	function secToHR($seconds) {
+	  $minutes = floor(($seconds / 60) % 60);
+	  $seconds = $seconds % 60;
+	  return "$minutes:$seconds";
+	}
+
+	clog('ChromePhp has been included on Songs.php...');
+	clog('clog() has been declared');
+	clog('secToHR() created successfully');
+	
+	// ASSIGN VARIABLE
+	// Playlist variables
+	if (!isset($_GET['albumID']))
+	{
+		die("Error, album id not specified...");
+		clog("Error, album id not specified...");
+	}
+	if (!isset($_GET['albumTitle']))
+	{
+		die("Error, album title not specified...");
+		clog("Error, album title not specified...");
+	}
+	if (!isset($_GET['genreID']))
+	{
+		die("Error, genre id not specified...");
+		clog("Error, genre id not specified...");
+	}
+	if (!isset($_GET['genreDesc']))
+	{
+		die("Error, genre description not specified...");
+		clog("Error, genre description not specified...");
+	}
+	if (!isset($_GET['artistID']))
+	{
+		die("Error, artist id not specified...");
+		clog("Error, artist id not specified...");
+	}
+	if (!isset($_GET['artistname']))
+	{
+		die("Error, artist name not specified...");
+		clog("Error, artist name not specified...");
+	}
+	// User variables
+	if (!isset($_GET['firstName']))
+	{
+		die("Error, first name not specified...");
+		clog("Error, first name not specified...");
+	}
+	if (!isset($_GET['lastName']))
+	{
+		die("Error, last name not specified...");
+		clog("Error, last name not specified...");
+	}
+	if (!isset($_GET['loginName']))
+	{
+		die("Error, login name not specified...");
+		clog("Error, login name not specified...");
+	}
+	if (!isset($_GET['city']))
+	{
+		die("Error, city not specified...");
+		clog("Error, city not specified...");
+	}
+	if (!isset($_GET['state']))
+	{
+		die("Error, state not specified...");
+		clog("Error, state not specified...");
+	}
+	if (!isset($_GET['email']))
+	{
+		die("Error, email not specified...");
+		clog("Error, email not specified...");
+	}
+	if (!isset($_GET['travelTime']))
+	{
+		die("Error, travel time not specified...");
+		clog("Error, travel time not specified...");
+	}
+	if (!isset($_GET['playlistTitle']))
+	{
+		die("Error, playlist title not specified...");
+		clog("Error, playlist title not specified...");
+	}
+
+	// ESCAPE ANY MALICIOUS CHARACTERS IN THE INPUT VARIABLE
+	// Playlist variables
+	$genreID 	= htmlspecialchars($_GET['genreID']);
+	$genreDesc 	= htmlspecialchars($_GET['genreDesc']);
+	$artistID 	= htmlspecialchars($_GET['artistID']);
+	$artistname = htmlspecialchars($_GET['artistname']);
+	$albumID 	= htmlspecialchars($_GET['albumID']);
+	$albumTitle = htmlspecialchars($_GET['albumTitle']);
+	// User variables
+	$firstName 	= htmlspecialchars($_GET['firstName']);
+	$lastName 	= htmlspecialchars($_GET['lastName']);
+	$loginName 	= htmlspecialchars($_GET['loginName']);
+	$city 		= htmlspecialchars($_GET['city']);
+	$state 		= htmlspecialchars($_GET['state']);
+	$email 		= htmlspecialchars($_GET['email']);
+	$travelTime = htmlspecialchars($_GET['travelTime']);
+	$playlistTitle = htmlspecialchars($_GET['playlistTitle']);
+
+	clog('All variables assigned...');
+	clog('GenreID = '.$genreID);
+	clog('GenreDesc = '.$genreDesc);
+	clog('ArtistID = '.$artistID);
+	clog('ArtistName = '.$artistname);
+	clog('AlbumID = '.$albumID);
+	clog('AlbumTitle = '.$albumTitle);
+	clog('PlaylistTitle = '.$playlistTitle);
+	
 	require "DBConnection.php";
 	$db = get_db();
 
-	// GET AND SET VARIABLES HERE
+		
 	
 ?>
 
