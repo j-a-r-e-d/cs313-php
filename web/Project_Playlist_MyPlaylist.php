52ae8acea0708e3cf32bf30b7e28d176b361a871
Jared K <kel03009@byui.edu>
2020-06-07 20:36:14 -0500

    .

--- web/Project_Playlist_MyPlaylist.php
+++ web/Project_Playlist_MyPlaylist.php
@@ -47,7 +47,7 @@
 		die("Error, artist name not specified...");
 		clog("Error, artist name not specified...");
 	}
-	if (!empty($_GET['songs[]']))
+	if (!empty($_GET['songs']))
 	{
 		die("Error, songs not specified...");
 		clog("Error, songs not specified...");
@@ -121,7 +121,6 @@
 	clog('AlbumID = '.$albumID);
 	clog('AlbumTitle = '.$albumTitle);
 	clog('PlaylistTitle = '.$playlistTitle);
-	clog('Songs[] = '.print_r($songs));
 	
 	require "DBConnection.php";
 	$db = get_db();
