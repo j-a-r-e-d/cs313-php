210a5aa1fb7d60fd1eb967e433746ed95a8ee3b0
Jared K <kel03009@byui.edu>
2020-06-07 20:54:23 -0500

    .

--- web/Project_Playlist_MyPlaylist.php
+++ web/Project_Playlist_MyPlaylist.php
@@ -102,7 +102,7 @@
 	$artistname = htmlspecialchars($_GET['artistname']);
 	$albumID 	= htmlspecialchars($_GET['albumID']);
 	$albumTitle = htmlspecialchars($_GET['albumTitle']);
-	$songs 		= array(htmlspecialchars($_GET['songs[]']));
+	// $songs 		= array(htmlspecialchars($_GET['songs[]']));
 	// User variables
 	$firstName 	= htmlspecialchars($_GET['firstName']);
 	$lastName 	= htmlspecialchars($_GET['lastName']);
@@ -172,12 +172,12 @@
 			
 			$cnt = 0;
 
-			if(!empty($songs)){
-				$checked_count = count($songs);
+			if(!empty($_GET['songs[]'])){
+				$checked_count = count($_GET['$songs[]');
 				echo "Total Tracks: ".$checked_count"<br>";
 			}
 
-			foreach ($_GET['songs'] as $selected) {
+			foreach ($_GET['songs[]'] as $selected) {
 				$cnt++;
 				echo $cnt.". ".$selected."<br>";
 			}
